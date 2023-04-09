4b54e3fe1616ff9312f50a3194e90f9f33cfdde7
Gabriele-Mangione <Gabriele.Mangione@hotmail.com>
2023-04-09 11:59:03 +0200

    a

--- LEaD-the-way.ino
+++ LEaD-the-way.ino
@@ -1,19 +1,19 @@
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
 #include <ArduinoOTA.h>
 
 #include <FastLED.h>
 
-#define SSID_WIFI "coldspot"
+#define SSID_WIFI "Coldspot"
 #define PW_WIFI "Hotstoppassword"
 
 #define FB_APIKEY "AIzaSyAHtWE38JhTMDdpVXOleN8jjWL9o6VFR0Q"
 #define FB_DATABASEURL "https://lead-the-way-dac45-default-rtdb.europe-west1.firebasedatabase.app/"
 
 
-#define NUM_LEDS 180
+#define NUM_LEDS 94
 #define RAINBOW_LENGHT 0.2  //sinus length factor for the whole stripe, 0-1
 
 //switch case
@@ -33,11 +33,13 @@ TaskHandle_t LEDTask;
 TaskHandle_t WiFiTask;
 
 //Global Variables
+CRGB leds[NUM_LEDS];
 float timeCounter;
-uint8_t LEDAdress = 0;
-float potValue;
 uint8_t deviceMode = 1;
-uint8_t brightnessInCode = 50;
+uint8_t brightnessInCode = 30;
+uint8_t red = 51;
+uint8_t green = 19;
+uint8_t blue = 1;
 
 uint8_t counter = 0;
 
@@ -45,7 +47,6 @@ uint8_t counter = 0;
 int easeOutCirc(int x);
 uint8_t getRandom(uint8_t maximum);
 uint8_t getFrequencyCount(uint8_t freq);
-uint8_t red, green, blue;
 
 /**
  * generates circular curve. 0-1000 is rising, 1000-2000 is falling
@@ -72,17 +73,19 @@ uint8_t getFrequencyCount(uint8_t freq) {
 void setup() {
   Serial.begin(115200);
 
-  xTaskCreatePinnedToCore(LEDProcess, "LED Task", 10000, NULL, 0, &LEDTask, 0);
-  xTaskCreatePinnedToCore(WiFiProcess, "WiFi Task", 10000, NULL, 0, &WiFiTask, 0);
+  xTaskCreatePinnedToCore(LEDProcess, "LED Task", 10000, NULL, 1, &LEDTask, 1);
+  xTaskCreatePinnedToCore(WiFiProcess, "WiFi Task", 20000, NULL, 0, &WiFiTask, 0);
 }
 
 void LEDProcess(void* pvParameters) {
+  Serial.print("LEDProcess running on core ");
+  Serial.println(xPortGetCoreID());
   pinMode(OD_PIN, OUTPUT);
 
-  CRGB leds[NUM_LEDS];
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 
   while (true) {
+    uint8_t LEDAdress = 0;
     switch (deviceMode) {
       case RAINBOW:
         timeCounter = fmod(((double)millis()) / 1000, (2 * PI));
@@ -113,6 +116,7 @@ void LEDProcess(void* pvParameters) {
         for (LEDAdress = 0; LEDAdress < NUM_LEDS; LEDAdress++) {  //for every LED on the stripe
           leds[LEDAdress] = CRGB(red, green, blue);
         }
+        break;
       case false:
       default:
         for (LEDAdress = 0; LEDAdress < NUM_LEDS; LEDAdress++) {  //for every LED on the stripe
@@ -120,11 +124,14 @@ void LEDProcess(void* pvParameters) {
         }
         break;
     }
+    //Serial.print("LED print!");
     FastLED.show();
   }
 }
 
 void WiFiProcess(void* pvParameters) {
+  Serial.print("WiFiProcess running on core ");
+  Serial.println(xPortGetCoreID());
   //WiFi Setup
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID_WIFI, PW_WIFI);
@@ -134,7 +141,7 @@ void WiFiProcess(void* pvParameters) {
     Serial.print(".");
     delay(1000);
   }
-  Serial.print("\nConnected!");
+  Serial.println("\nConnected!");
 
   //Firebase Setup
   FirebaseData fbdo;
@@ -157,6 +164,7 @@ void WiFiProcess(void* pvParameters) {
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+  Serial.println("Firebase begin!");
 
   //OTA Setup
   ArduinoOTA.setHostname("LEaD-the-way");
@@ -189,9 +197,19 @@ void WiFiProcess(void* pvParameters) {
     });
 
   ArduinoOTA.begin();
+  Serial.println("OTA begin!");
 
   while (true) {
+    //Serial.println("WiFiTask looping!");
     ArduinoOTA.handle();
+    if (Firebase.ready() && signupOK) {
+      Firebase.RTDB.setInt(&fbdo, "Strip1/brightness", 50);
+      Firebase.RTDB.getInt(&fbdo, "Strip1/brightness");
+      Serial.print(fbdo.intData());
+      if (Firebase.RTDB.getInt(&fbdo, "Strip1/deviceMode")) {
+        deviceMode = fbdo.intData();
+      }
+    }
   }
 }
 
